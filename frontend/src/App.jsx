48ba0df8c24d8ac17aa3720df6e01f3600216723
Jefferson Quinto <jeffersonquinto2023@email.com>
2025-07-26 00:34:35 +0800

    Fresh restart: Recreated frontend folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { useEffect, useState } from 'react'
 import './App.css'
 
 const lyrics = [
